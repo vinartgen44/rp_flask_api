66bc8943207055fcd5e60259664954723308ee5d
Artem Vinogradov <vinartgen@outlook.com>
2024-11-22 12:01:57 +0300

    add SQLAlchemy

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+import config
 from flask import render_template
-import connexion
+from models import Person
 
-
-app = connexion.App(__name__, specification_dir="./")
-
-app.add_api("swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
--- people.py
+++ people.py
@@ -1,64 +1,44 @@
-from datetime import datetime
 from flask import abort, make_response
+from config import db
+from models import Person, PersonSchema, people_schema
 
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
 
-PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    },
-}
-
-
 def read_all():
-    return list(PEOPLE.values())
+    people = Person.query.all()
+    return people_schema.dump(people)
 
 
 def create(person):
     lname = person.get("lname")
-    fname = person.get("fname", "")
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
-    if lname and lname not in PEOPLE:
-        PEOPLE[lname] = {
-            "lname": lname,
-            "fname": fname,
-            "timestamp": get_timestamp(),
-        }
-        return PEOPLE[lname], 201
+    if existing_person is None:
+        new_person = person_schema.load(person, session=db.session)
+        db.session.add(new_person)
+        db.session.commit()
     else:
-        abort(
-            406,
-            f"Person with last name {lname} already exists",
-        )
+        abort(409, f"Person with last name {lname} already exists")
 
 
 def read_one(lname):
-    if lname in PEOPLE:
-        return PEOPLE[lname]
+    person = Person.query.filter(Person.lname == lname).one_or_none()
+    if person is not None:
+        return person_schema.dump(person)
     else:
         abort(404, f"Person with last name {lname} not found")
 
 
 def update(lname, person):
-    if lname in PEOPLE:
-        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
-        PEOPLE[lname]["timestamp"] = get_timestamp()
-        return PEOPLE[lname]
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+    if existing_person:
+        update_person = person_schema.load(person, session=db.session)
+        existing_person.fname = update_person.fname
+        db.session.merge(existing_person)
+        db.session.commit()
     else:
         abort(
             404,
@@ -67,14 +47,10 @@ def update(lname, person):
 
 
 def delete(lname):
-    if lname in PEOPLE:
-        del PEOPLE[lname]
-        return make_response(
-            f"Person with last name {lname} deleted",
-            200,
-        )
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+    if existing_person:
+        db.session.delete(existing_person)
+        db.session.commit()
+        return make_response(f"Person with last name {lname} deleted", 200)
     else:
-        abort(
-            404,
-            f"Person with last name {lname} not found",
-        )
+        abort(404, f"Person with last name {lname} not found")
